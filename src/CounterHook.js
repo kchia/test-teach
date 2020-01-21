b463d2de2901a209fb81fc8f3d35502ec79f97dc
kchia <kchia87@gmail.com>
2020-01-21 14:54:09 -0500

    step 4: display message

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -4,7 +4,11 @@ import React, { useState } from 'react';
 function Counter() {
   // declare count variable and set it to 0
   let [count, setCount] = useState(0); // useState accepts the initial state as the only argument and returns the current state and the function that updates the count state
-  return <div></div>;
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+    </div>
+  );
 }
 
 export default Counter;
