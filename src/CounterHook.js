4d140c12e3676f685407c515be74a833fa3fb412
kchia <kchia87@gmail.com>
2020-01-21 14:52:57 -0500

    step 3: initialize count state

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 
 function Counter() {
+  // declare count variable and set it to 0
+  let [count, setCount] = useState(0); // useState accepts the initial state as the only argument and returns the current state and the function that updates the count state
   return <div></div>;
 }
 
