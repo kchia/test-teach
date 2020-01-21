8d025f8ec25edcd833ef249f23b9f6fd2ef41dda
kchia <kchia87@gmail.com>
2020-01-21 15:01:09 -0500

    step 5: add click handler

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -4,9 +4,14 @@ import React, { useState } from 'react';
 function Counter() {
   // declare count variable and set it to 0
   let [count, setCount] = useState(0); // useState accepts the initial state as the only argument and returns the current state and the function that updates the count state
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div>
       <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me </button>
     </div>
   );
 }
