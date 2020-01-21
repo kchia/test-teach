0af6a9e3c92ad5ffa3cfaf049c9b13765767d748
kchia <kchia87@gmail.com>
2020-01-21 15:04:53 -0500

    add destructuring comment

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -5,6 +5,11 @@ function Counter() {
   // declare count variable and set it to 0
   let [count, setCount] = useState(0); // useState accepts the initial state as the only argument and returns the current state and the function that updates the count state
 
+  // Lines 9 - 11 perform the equivalent of line 6 (which uses array destructuring syntax)
+  // let countVariable = useState(0); // Returns a pair
+  // let count = countVariable[0]; // First item in a pair
+  // let setCount = countVariable[1]; // Second item in a pair
+
   function handleClick() {
     setCount(count + 1);
   }
